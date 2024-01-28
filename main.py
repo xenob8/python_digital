88c10f20d40e5f5a77c46844201bfec633cb240b
Sergey Solodovnikov <90275969+xenob8@users.noreply.github.com>
2024-01-28 20:28:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages):
+        if not isinstance(pages, int):
+            raise TypeError("pages must be int type")
+        if pages < 0:
+            raise ValueError("pages must be >=0")
+        self._pages = pages
+
+    def __str__(self):
+        return f"{super().__str__()} Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration):
+        if not isinstance(duration, float):
+            raise TypeError("duration must be float type")
+        if duration < 0:
+            raise ValueError("duration must be >=0")
+        self._duration = duration
+
+    def __str__(self):
+        return f"{super().__str__()} Длительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
